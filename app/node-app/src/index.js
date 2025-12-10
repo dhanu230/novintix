3df0adf3d331e1467b1048dd42243eaf531f7502
dhanu230 <dhanuram2310@gmail.com>
2025-12-11 01:04:29 +0530

    fixed vulnarabilities

--- app/node-app/src/index.js
+++ app/node-app/src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< f1062d418a52f0bc3159ce6b6da1b6293b2535f5
 const express = require("express");
 const app = express();
 
@@ -10,3 +11,17 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+=======
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!", version: "v1" });
+});
+
+app.get("/healthz", (req, res) => res.send("ok"));
+app.get("/readyz", (req, res) => res.send("ready"));
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+>>>>>>> fixed vulnarabilities
