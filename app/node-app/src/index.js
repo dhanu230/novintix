a389a93d47aa198cf895086f78f416fae51aba1f
dhanu230 <159844219+dhanu230@users.noreply.github.com>
2025-12-11 00:39:40 +0530

    Update index.js-1

--- app/node-app/src/index.js
+++ app/node-app/src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World!", version: "v1" });
+    res.send("Novintix App running successfully!");
 });
 
-app.get("/healthz", (req, res) => res.send("ok"));
-app.get("/readyz", (req, res) => res.send("ready"));
-
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
