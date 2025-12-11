16ea29d52a2d0cec1d8312000526a2d8f1857d5f
dhanu230 <dhanuram2310@gmail.com>
2025-12-11 18:33:57 +0530

    fixed

--- iac/terraform/Eks.tf
+++ iac/terraform/Eks.tf
@@ -2,20 +2,38 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
 
-  cluster_name    = var.cluster_name
+  cluster_name    = "demo-eks"
   cluster_version = "1.29"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  control_plane_subnet_ids       = module.vpc.private_subnets
+  create_iam_role                = true
 
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
-
-
   cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
   authentication_mode = "API_AND_CONFIG_MAP"
 
+  enable_irsa = true
+
+  access_entries = {
+  admin_user = {
+    principal_arn = "arn:aws:iam::037776138029:user/dhanu"
+
+    policy_associations = {
+      cluster_admin = {
+        policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+        access_scope = {
+          type = "cluster"
+        }
+      }
+    }
+  }
+}
+
+  
   eks_managed_node_groups = {
     default = {
       min_size     = 1
