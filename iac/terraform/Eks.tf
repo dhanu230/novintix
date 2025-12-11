97d8af5cf3f7af3b21dda48bd10745e466115a42
dhanu230 <dhanuram2310@gmail.com>
2025-12-11 15:51:51 +0530

    k8s

--- iac/terraform/Eks.tf
+++ iac/terraform/Eks.tf
@@ -8,12 +8,28 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
+  authentication_mode = "API_AND_CONFIG_MAP"
+
   eks_managed_node_groups = {
     default = {
       min_size     = 1
       max_size     = 3
       desired_size = 2
       instance_types = ["t3.small"]
+
+      iam_role_additional_policies = {
+        ecr_pull_access       = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+        cloudwatch_logs       = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+        cloudwatch_full       = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+        secretsmanager_access = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+        amp_access            = "arn:aws:iam::aws:policy/AmazonPrometheusRemoteWriteAccess"
+      }
     }
   }
 }
